3deaa6bdbdcd8bc512f626be541c458dfb66df43
thndrbrrr <thndrbrrr@gmail.com>
2025-10-21 21:43:52 -0700

    chore: bump version to 0.2.0
    Changing the parameter order of a public function deserves
    a version change.

--- gtasks.el
+++ gtasks.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: thndrbrrr@gmail.com
 ;; Maintainer: thndrbrrr@gmail.com
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; URL: https://github.com/thndrbrrr/gtasks
 ;; Keywords: convenience, tools, google, tasks, api
